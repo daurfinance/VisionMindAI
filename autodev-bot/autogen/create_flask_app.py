a20169a91665354a36739e91845ad585c2e25f89
daurfinance <83969321+daurfinance@users.noreply.github.com>
2025-04-09 14:20:27 +0500

    Update create_flask_app.py

--- autodev-bot/autogen/create_flask_app.py
+++ autodev-bot/autogen/create_flask_app.py
@@ -1,3 +1,4 @@
+Конечно! Вот пример минимального Flask-приложения с одним маршрутом /hello:
 ```python
 from flask import Flask, jsonify
 
@@ -10,3 +11,5 @@ def hello():
 if __name__ == '__main__':
     app.run()
 ```
+
+Сохраните код в файле `app.py` и запустите его. После этого вы можете отправить GET-запрос на http://127.0.0.1:5000/hello и получите JSON `{"message": "Hello from bot!"}` в ответ.
